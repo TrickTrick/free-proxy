95075e3f40168ec11c4db8563d7f1ad0def96e92
TrickTrick <alexeymarkov.x7@gmail.com>
2018-01-09 21:09:32 +0200

    fix

--- FreeProxy.php
+++ FreeProxy.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace tricktrick\freeProxy;
+namespace tricktrick;
 
 use phpQuery;
 
@@ -9,7 +9,7 @@ class FreeProxy
     protected static $_url = 'https://free-proxy-list.net';
     protected static $_ips;
     protected static $_counter = 0;
-    protected static $_country = null;
+//    protected static $_country = null;
 
     public static function getIps()
     {
@@ -22,8 +22,8 @@ class FreeProxy
             if(!$k) continue;
             $ip = pq($tr)->find('td:eq(0)')->html();
             $port = pq($tr)->find('td:eq(1)')->html();
-            $country = pq($tr)->find('td:eq(2)')->html();
-            if (self::$_country && $country != self::$_country) continue;
+//            $country = pq($tr)->find('td:eq(2)')->html();
+//            if (self::$_country && $country != self::$_country) continue;
             if ($ip && $port)
                 $res[] = [
                     'ip' => $ip,
